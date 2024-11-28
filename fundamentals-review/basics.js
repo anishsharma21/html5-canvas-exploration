8ce00aca24c4df4dcf4ee2e4042187d4e31235da
anishsharma21 <anisharma2103@gmail.com>
2024-11-28 11:48:29 +1100

    Reviewed basics of JS

--- fundamentals-review/basics.js
+++ fundamentals-review/basics.js
@@ -4,19 +4,46 @@ let isAdult = true;
 let nullValue = null;
 let undefinedVal;
 let person = {
-    fullName: fullName,
-    age: age,
-}
+  fullName: fullName,
+  age: age,
+};
 
 console.log(age);
 console.log(fullName);
 console.log(person);
 
 function swap(a, b) {
-    return [b, a]
+  return [b, a];
 }
 
-a = 1
-b = 2
-console.log(a, b)
-console.log(swap(a, b).toString().replace(",", " "))
+a = 1;
+b = 2;
+console.log(a, b);
+console.log(swap(a, b).toString().replace(",", " "));
+
+function circleArea(radius) {
+  return Math.PI * radius ** 2;
+}
+
+radius = 2;
+console.log(
+  `Area of circle with radius ${radius}: ${
+    Math.round(circleArea(radius) * 100) / 100
+  }`
+);
+
+for (let i = 0; i < 5; i++) {
+  console.log(`Round ${i + 1}`);
+}
+
+function reverseString(s) {
+  rs = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    rs += s[i];
+  }
+  return rs;
+}
+
+mystr = "hello";
+console.log(reverseString(mystr));
+console.log(mystr.split("").reverse().join(""));
